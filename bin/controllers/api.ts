e2ff54c10dc0ba314920c915bf9261410d10ff35
raidenShadow <alirezakiaei@gmail.com>
2020-10-17 19:36:38 +0330

    validate fetch api

--- bin/controllers/api.ts
+++ bin/controllers/api.ts
@@ -8,7 +8,7 @@ class ApiController {
     saveLink: RequestHandler = async (req, res) => {
         try {
             const stats = new Statistics(req.body);
-            await ApiService.saveLink(stats);
+            const fetch: Fetch = await ApiService.saveLink(stats);
             return res.status(200).send({ stats });
         } catch (error) {
             return res.status(400).send({ error: error.message });
@@ -17,12 +17,8 @@ class ApiController {
 
     saveSite: RequestHandler = async (req, res) => {
         try {
-            console.log(req.body);
-            
             const stats = new Statistics(req.body);
             const fetch: Fetch = await ApiService.saveSite(stats);
-            console.log(fetch.parsed_response);
-            
             return res.status(200).send({ message: 'Saved successfully' });
         } catch (error) {
             return res.status(400).send({ error: error.message });
